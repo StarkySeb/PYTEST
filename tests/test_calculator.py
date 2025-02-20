11a2224dcbee204bfc5ac2e5590172a419a0e572
StarkySeb <sebastian.stary@student.vitkovickastredni.cz>
2025-02-20 12:32:38 +0100

    vsechno

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,3 +6,15 @@ def test_add():
 
 def test_addwrong():
     assert calculator.add_wrong(5,3) == 8
+
+def test_subtract():
+    assert calculator.subtract(5,3) == 2
+
+def test_multiply():
+    assert calculator.multiply(5,3) == 15
+
+def test_multiplywrong():
+    assert calculator.multiply_wrong(5,3) == 15
+
+def test_divide():
+    assert calculator.divide(8,2) == 4
