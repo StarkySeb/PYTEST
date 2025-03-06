cdc14afbe93ca4c273592bc754d8b4d9c72d68b0
StarkySeb <sebastian.stary@student.vitkovickastredni.cz>
2025-03-06 11:49:56 +0100

    stashed

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,8 +4,21 @@ from src import calculator
 def test_add():
     assert calculator.add(5,3) == 8
 
+def test_add2():
+    assert calculator.add(1,2) == 3
+    assert calculator.add(0,5) == 5
+    assert calculator.add(-2,9) == 7
+    assert calculator.add(0,0) == 0
+
+
 def test_addwrong():
     assert calculator.add_wrong(5,3) == 8
+    
+def test_addwrong2():
+    assert calculator.add_wrong(1,2) == 3
+    assert calculator.add_wrong(0,5) == 5
+    assert calculator.add_wrong(-2,9) == 7
+    assert calculator.add_wrong(0,0) == 0
 
 def test_subtract():
     assert calculator.subtract(5,3) == 2
@@ -19,3 +32,18 @@ def test_multiplywrong():
 def test_divide():
     assert calculator.divide(8,2) == 4
 
+<<<<<<< Updated upstream
+=======
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (1, 2, 3),
+        (0, 5, 5),
+        (-2, 9, 7),
+        (5, -8, -3),
+    ],
+)
+
+def test_add_parametrized(a, b, expected):
+    assert calculator.add(a, b) == expected
+>>>>>>> Stashed changes
